33f083f6d10bd81dbf22006934235ff11ce06aa3
imkeisuke <166368235+imkeisuke@users.noreply.github.com>
2024-07-20 17:13:07 +0900

    Update cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -6,31 +6,34 @@ use clap::{Parser, ValueEnum};
     version, author, about,
     arg_required_else_help = true,
 )]
-//コマンドラインツールのオプション解析
-
 pub struct CliOpts {
-  //MODE
-  #[arg(short,long,default_value_t = RunMode::Auto, value_name = "MODE", required = false, ignore_case = true, value_enum, help = "Mode of operation.")]
-  pub mode: RunMode,
-  //抽出結果の保存先
-  #[arg(short, long, default_value = ".", value_name = "DEST", required = false, help = "Destination of the extraction results.")]
-  pub dest: Option<PathBuf>,
-  #[arg(short, long, default_value = "Comon.zip", value_name = "OUTPUT", required = false, help = "Output file for the archive.")]
-  pub output: Option<PathBuf>,
-  #[arg(short, long = "no-recursive", help = "No reursiven mode.", default_value_t =false)]
-  pub no_recursive: bool,
-  #[arg(short,long, help = "Display verbose output.", default_value_t = false)]
-  pub verbose: bool,
-  #[arg(value_name = "ARGUMENTS", help = "List of files or direstories to be processed.", long_help = "extract mode: archive files to be extracted. archive mode: files to be archived. auto mode: if the arguments have archive files, it will extract them. Otherwise, it will archive the files.")]
-  pub args: Vec<PathBuf>,
-  #[arg(short, long, help = "Extract to a directory named after the archive file.")]
-  pub to_archive_name_dir: bool,
+    #[arg(short, long, default_value_t = RunMode::Auto, value_enum, help = "Mode of operation.")]
+    pub mode: RunMode,
+
+    #[arg(short, long, default_value = ".", help = "Destination of the extraction results.")]
+    pub dest: Option<PathBuf>,
+
+    #[arg(short, long, default_value = "Comon.zip", help = "Output file for the archive.")]
+    pub output: Option<PathBuf>,
+
+    #[arg(short, long, help = "No recursive mode.", default_value_t = false)]
+    pub no_recursive: bool,
+
+    #[arg(short, long, help = "Display verbose output.", default_value_t = false)]
+    pub verbose: bool,
+
+    #[arg(help = "List of files or directories to be processed.")]
+    pub args: Vec<PathBuf>,
+
+    #[arg(short, long, help = "Extract to a directory named after the archive file.")]
+    pub to_archive_name_dir: bool,
 }
+
 #[derive(Debug, Clone, ValueEnum, PartialEq)]
 pub enum RunMode {
-  Auto,
-  Archive, 
-  Extract,
+    Auto,
+    Archive,
+    Extract,
 }
 
 #[derive(Debug)]
