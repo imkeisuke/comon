9d75079fe1cd438d6f7b24c2855065d47842f1e8
imkeisuke <166368235+imkeisuke@users.noreply.github.com>
2024-07-20 17:24:37 +0900

    Update rar.rs

--- src/archiver/rar.rs
+++ src/archiver/rar.rs
@@ -1,22 +1,42 @@
-use crate::cli::Result;
-use crate::format::Format;
-use crate::ArchiverOpts;
+use crate::archiver::{Archiver, Format, ArchiverOpts};
+use crate::cli::{ToteError, Result};
 
-pub struct RarArchiver;
+pub(super) struct RarArchiver {
+}
 
-impl RarArchiver {
-    pub fn new() -> Self {
-        Self
+impl Archiver for  RarArchiver {
+    fn perform(&self, _: &ArchiverOpts) -> Result<()> {
+        Err(ToteError::UnsupportedFormat("only extraction support for rar".to_string()))
+    }
+    fn format(&self) -> Format {
+        Format::Rar
     }
 }
 
-impl crate::Archiver for RarArchiver {
-    fn perform(&self, inout: &ArchiverOpts) -> Result<()> {
-        // RARアーカイブの処理を実装
-        Ok(())
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::path::PathBuf;
+    use crate::verboser::create_verboser;
+
+    #[test]
+    fn test_format() {
+        let archiver = RarArchiver{};
+        assert_eq!(archiver.format(), Format::Rar);
     }
 
-    fn format(&self) -> Format {
-        Format::Rar
+    #[test]
+    fn test_archive() {
+        let archiver = RarArchiver{};
+        let opts = ArchiverOpts {
+            dest: PathBuf::from("results/test.rar"),
+            targets: vec![],
+            overwrite: false,
+            recursive: false,
+            v: create_verboser(false),
+        };
+        let r = archiver.perform(&opts);
+        assert!(r.is_err());
     }
 }
