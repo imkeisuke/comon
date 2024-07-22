dcb728bda0eed16d7e967dfd69d0ca8a15f15a04
imkeisuke <166368235+imkeisuke@users.noreply.github.com>
2024-07-22 20:39:39 +0900

    Update tote_error.rs

--- src/tote_error.rs
+++ src/tote_error.rs
@@ -1,42 +1,32 @@
-// src/tote_error.rs
 use std::fmt;
-use std::io;
-use zip::result::ZipError;
-use walkdir::Error as WalkDirError;
 
 #[derive(Debug)]
 pub enum ToteError {
-    IoError(io::Error),
-    ZipError(ZipError),
-    WalkDirError(WalkDirError),
+    IoError(std::io::Error),
+    ZipError(zip::ZipError),
+    FormatError(String),
     UnknownFormat(String),
 }
 
 impl fmt::Display for ToteError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             ToteError::IoError(err) => write!(f, "IO error: {}", err),
-            ToteError::ZipError(err) => write!(f, "ZIP error: {}", err),
-            ToteError::WalkDirError(err) => write!(f, "WalkDir error: {}", err),
-            ToteError::UnknownFormat(err) => write!(f, "Unknown format: {}", err),
+            ToteError::ZipError(err) => write!(f, "Zip error: {}", err),
+            ToteError::FormatError(err) => write!(f, "Format error: {}", err),
+            ToteError::UnknownFormat(fmt) => write!(f, "Unknown format: {}", fmt),
         }
     }
 }
 
-impl From<io::Error> for ToteError {
-    fn from(err: io::Error) -> ToteError {
-        ToteError::IoError(err)
+impl From<std::io::Error> for ToteError {
+    fn from(error: std::io::Error) -> Self {
+        ToteError::IoError(error)
     }
 }
 
-impl From<ZipError> for ToteError {
-    fn from(err: ZipError) -> ToteError {
-        ToteError::ZipError(err)
-    }
-}
-
-impl From<WalkDirError> for ToteError {
-    fn from(err: WalkDirError) -> ToteError {
-        ToteError::WalkDirError(err)
+impl From<zip::ZipError> for ToteError {
+    fn from(error: zip::ZipError) -> Self {
+        ToteError::ZipError(error)
     }
 }
