9df9f022223808a8c7a9adff8b956bd3d83aeeea
imkeisuke <166368235+imkeisuke@users.noreply.github.com>
2024-07-22 20:39:13 +0900

    Update format.rs

--- src/format.rs
+++ src/format.rs
@@ -1,42 +1,34 @@
-// src/format.rs
-use crate::tote_error::ToteError;
 use std::fmt;
+use crate::tote_error::ToteError;
 
-#[derive(Debug)]
 pub enum Format {
     Tar,
     TarGz,
     TarBz2,
-    TarXz,
     Zip,
 }
 
 impl fmt::Display for Format {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
             Format::Tar => write!(f, "tar"),
             Format::TarGz => write!(f, "tar.gz"),
             Format::TarBz2 => write!(f, "tar.bz2"),
-            Format::TarXz => write!(f, "tar.xz"),
             Format::Zip => write!(f, "zip"),
         }
     }
 }
 
-pub fn get_format_from_extension(ext: &str) -> Result<Format, ToteError> {
-    match ext {
-        "tar" => Ok(Format::Tar),
-        "gz" => Ok(Format::TarGz),
-        "bz2" => Ok(Format::TarBz2),
-        "xz" => Ok(Format::TarXz),
-        "zip" => Ok(Format::Zip),
-        _ => Err(ToteError::UnknownFormat(ext.to_string())),
-    }
-}
-
-pub fn get_format_from_path(path: &std::path::Path) -> Result<Format, ToteError> {
-    match path.extension().and_then(|s| s.to_str()) {
-        Some(ext) => get_format_from_extension(ext),
-        None => Err(ToteError::UnknownFormat("No extension found".to_string())),
+pub fn detect_format(filename: &str) -> Result<Format, ToteError> {
+    if filename.ends_with(".tar") {
+        Ok(Format::Tar)
+    } else if filename.ends_with(".tar.gz") || filename.ends_with(".tgz") {
+        Ok(Format::TarGz)
+    } else if filename.ends_with(".tar.bz2") || filename.ends_with(".tbz2") {
+        Ok(Format::TarBz2)
+    } else if filename.ends_with(".zip") {
+        Ok(Format::Zip)
+    } else {
+        Err(ToteError::UnknownFormat(filename.to_string()))
     }
 }
