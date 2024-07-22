be5bab5b082b29321d0c5046b1ef4ac5e82e8feb
imkeisuke <166368235+imkeisuke@users.noreply.github.com>
2024-07-22 20:32:46 +0900

    Update tar.rs

--- src/archiver/tar.rs
+++ src/archiver/tar.rs
@@ -1,135 +1,66 @@
+// src/archiver/tar.rs
 use std::fs::File;
 use std::io::{self, Write};
 use std::path::{Path, PathBuf};
+use tar::{Builder, Archive};
 use flate2::Compression;
 use flate2::write::GzEncoder;
-use tar::{Builder, Archive};
-
-use crate::archiver::{Archiver, ArchiverOpts};
-use crate::format::Format;
+use flate2::write::BzEncoder;
+use flate2::write::ZlibEncoder;
+use xz2::write::XzEncoder;
 use crate::tote_error::ToteError;
 
-pub struct TarArchiver;
-
-impl Archiver for TarArchiver {
-    fn perform(&self, inout: &ArchiverOpts) -> Result<(), ToteError> {
-        let file = inout.destination()?;
-        let tar_gz = GzEncoder::new(file, Compression::default());
-        let mut tar = Builder::new(tar_gz);
-
-        for path in &inout.targets {
-            if path.is_dir() {
-                tar.append_dir_all(path.file_name().unwrap(), path)?;
-            } else {
-                tar.append_path(path)?;
-            }
-        }
-
-        tar.finish()?;
-        Ok(())
-    }
-
-    fn format(&self) -> Format {
-        Format::Tar
-    }
+pub fn create_tar_gz_archive(src_dir: &Path, tar_gz_path: &Path) -> Result<(), ToteError> {
+    let tar_gz = File::create(tar_gz_path)?;
+    let enc = GzEncoder::new(tar_gz, Compression::default());
+    let mut tar = Builder::new(enc);
+    tar.append_dir_all(".", src_dir)?;
+    Ok(())
 }
 
-pub struct TarGzArchiver;
-
-impl Archiver for TarGzArchiver {
-    fn perform(&self, inout: &ArchiverOpts) -> Result<(), ToteError> {
-        let file = inout.destination()?;
-        let tar_gz = GzEncoder::new(file, Compression::default());
-        let mut tar = Builder::new(tar_gz);
-
-        for path in &inout.targets {
-            if path.is_dir() {
-                tar.append_dir_all(path.file_name().unwrap(), path)?;
-            } else {
-                tar.append_path(path)?;
-            }
-        }
-
-        tar.finish()?;
-        Ok(())
-    }
-
-    fn format(&self) -> Format {
-        Format::TarGz
-    }
+pub fn create_tar_bz2_archive(src_dir: &Path, tar_bz2_path: &Path) -> Result<(), ToteError> {
+    let tar_bz2 = File::create(tar_bz2_path)?;
+    let enc = BzEncoder::new(tar_bz2, Compression::default());
+    let mut tar = Builder::new(enc);
+    tar.append_dir_all(".", src_dir)?;
+    Ok(())
 }
 
-pub struct TarBz2Archiver;
-
-impl Archiver for TarBz2Archiver {
-    fn perform(&self, inout: &ArchiverOpts) -> Result<(), ToteError> {
-        let file = inout.destination()?;
-        let tar_bz2 = flate2::write::BzEncoder::new(file, Compression::default());
-        let mut tar = Builder::new(tar_bz2);
-
-        for path in &inout.targets {
-            if path.is_dir() {
-                tar.append_dir_all(path.file_name().unwrap(), path)?;
-            } else {
-                tar.append_path(path)?;
-            }
-        }
-
-        tar.finish()?;
-        Ok(())
-    }
-
-    fn format(&self) -> Format {
-        Format::TarBz2
-    }
+pub fn create_tar_xz_archive(src_dir: &Path, tar_xz_path: &Path) -> Result<(), ToteError> {
+    let tar_xz = File::create(tar_xz_path)?;
+    let enc = XzEncoder::new(tar_xz, Compression::default());
+    let mut tar = Builder::new(enc);
+    tar.append_dir_all(".", src_dir)?;
+    Ok(())
 }
 
-pub struct TarXzArchiver;
-
-impl Archiver for TarXzArchiver {
-    fn perform(&self, inout: &ArchiverOpts) -> Result<(), ToteError> {
-        let file = inout.destination()?;
-        let tar_xz = xz2::write::XzEncoder::new(file, 9);
-        let mut tar = Builder::new(tar_xz);
-
-        for path in &inout.targets {
-            if path.is_dir() {
-                tar.append_dir_all(path.file_name().unwrap(), path)?;
-            } else {
-                tar.append_path(path)?;
-            }
-        }
-
-        tar.finish()?;
-        Ok(())
-    }
-
-    fn format(&self) -> Format {
-        Format::TarXz
-    }
+pub fn extract_tar_archive(tar_path: &Path, dst_dir: &Path) -> Result<(), ToteError> {
+    let tar = File::open(tar_path)?;
+    let mut archive = Archive::new(tar);
+    archive.unpack(dst_dir)?;
+    Ok(())
 }
 
-pub struct TarZstdArchiver;
+pub fn extract_tar_gz_archive(tar_gz_path: &Path, dst_dir: &Path) -> Result<(), ToteError> {
+    let tar_gz = File::open(tar_gz_path)?;
+    let dec = GzDecoder::new(tar_gz);
+    let mut archive = Archive::new(dec);
+    archive.unpack(dst_dir)?;
+    Ok(())
+}
 
-impl Archiver for TarZstdArchiver {
-    fn perform(&self, inout: &ArchiverOpts) -> Result<(), ToteError> {
-        let file = inout.destination()?;
-        let tar_zstd = zstd::stream::write::Encoder::new(file, 0)?;
-        let mut tar = Builder::new(tar_zstd);
+pub fn extract_tar_bz2_archive(tar_bz2_path: &Path, dst_dir: &Path) -> Result<(), ToteError> {
+    let tar_bz2 = File::open(tar_bz2_path)?;
+    let dec = BzDecoder::new(tar_bz2);
+    let mut archive = Archive::new(dec);
+    archive.unpack(dst_dir)?;
+    Ok(())
+}
 
-        for path in &inout.targets {
-            if path.is_dir() {
-                tar.append_dir_all(path.file_name().unwrap(), path)?;
-            } else {
-                tar.append_path(path)?;
-            }
-        }
-
-        tar.finish()?;
-        Ok(())
-    }
-
-    fn format(&self) -> Format {
-        Format::TarZstd
-    }
+pub fn extract_tar_xz_archive(tar_xz_path: &Path, dst_dir: &Path) -> Result<(), ToteError> {
+    let tar_xz = File::open(tar_xz_path)?;
+    let dec = XzDecoder::new(tar_xz);
+    let mut archive = Archive::new(dec);
+    archive.unpack(dst_dir)?;
+    Ok(())
 }
