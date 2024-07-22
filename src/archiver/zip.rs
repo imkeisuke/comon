dce6b27b10477bd3c0d1e77010967d1ed5603f01
imkeisuke <166368235+imkeisuke@users.noreply.github.com>
2024-07-22 20:35:56 +0900

    Update zip.rs

--- src/archiver/zip.rs
+++ src/archiver/zip.rs
@@ -1,46 +1,14 @@
+// src/archiver/zip.rs
 use std::fs::File;
 use std::io::{self, Write};
 use std::path::{Path, PathBuf};
+use walkdir::WalkDir;
 use zip::write::FileOptions;
-use zip::ZipWriter;
-use crate::archiver::{Archiver, ArchiverOpts};
-use crate::format::Format;
 use crate::tote_error::ToteError;
 
-pub struct ZipArchiver;
+pub fn create_zip_archive(src_dir: &Path, zip_path: &Path) -> Result<(), ToteError> {
+    let zip_file = File::create(zip_path)?;
+    let mut zip = zip::ZipWriter::new(zip_file);
+    let options = FileOptions::default().compression_method(zip::CompressionMethod::Deflated);
 
-impl Archiver for ZipArchiver {
-    fn perform(&self, inout: &ArchiverOpts) -> Result<(), ToteError> {
-        let file = inout.destination()?;
-        let mut zip = ZipWriter::new(file);
-
-        for path in &inout.targets {
-            if path.is_dir() {
-                let options = FileOptions::default().compression_method(zip::CompressionMethod::Stored);
-                for entry in walkdir::WalkDir::new(path) {
-                    let entry = entry?;
-                    let path = entry.path();
-                    if path.is_dir() {
-                        zip.add_directory(path.to_string_lossy(), options)?;
-                    } else {
-                        zip.start_file(path.to_string_lossy(), options)?;
-                        let mut f = File::open(path)?;
-                        io::copy(&mut f, &mut zip)?;
-                    }
-                }
-            } else {
-                let options = FileOptions::default().compression_method(zip::CompressionMethod::Stored);
-                zip.start_file(path.to_string_lossy(), options)?;
-                let mut f = File::open(path)?;
-                io::copy(&mut f, &mut zip)?;
-            }
-        }
-
-        zip.finish()?;
-        Ok(())
-    }
-
-    fn format(&self) -> Format {
-        Format::Zip
-    }
-}
+    for entry in WalkDir::new(src_dir
