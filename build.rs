eba9e54c7ee58e3ae546238559308a7a6c7179c7
imkeisuke <166368235+imkeisuke@users.noreply.github.com>
2024-07-22 20:06:02 +0900

    Update build.rs

--- build.rs
+++ build.rs
@@ -5,22 +5,22 @@ use std::path::Path;
 
 include!("src/cli.rs");
 const APP_NAME: &str = "comon";
-fn generate(s: Shell, app: &mut Command, outdir: &Path, file: &str){
-  let destfile = outdir.join(file);
-  println!("dest: {}", destfile.display());
-  std::fs::create_dir_all(destfile.parent().unwrap()).unwrap();
-  let mut dest = File::create(destfile).unwrap();
-  clap_complete::generate(s, app, APP_NAME, &mut dest);
+
+fn generate(s: Shell, app: &mut Command, outdir: &Path, file: &str) {
+    let destfile = outdir.join(file);
+    println!("dest: {}", destfile.display());
+    std::fs::create_dir_all(destfile.parent().unwrap()).unwrap();
+    let mut dest = File::create(destfile).unwrap();
+    clap_complete::generate(s, app, APP_NAME, &mut dest);
 }
 
 fn main() {
-let mut app = CliOpts::command();
-app.set_bin_name(APP_NAME);
-let outdir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("target/completions/");
-generate(Shell::Bash, &mut app, &outdir, "bash/comon");
-generate(Shell::Elvish, &mut app, &outdir, "elvish/comon");
-generate(Shell::Fish, &mut app, &outdir, "fish/comon");
-generate(Shell::PowerShell, &mut app, &outdir, "powershell/comon");
-generate(Shell::Zsh, &mut app, &outdir, "zsh/_comon");
+    let mut app = CliOpts::command();
+    app.set_bin_name(APP_NAME);
+    let outdir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("target/completions/");
+    generate(Shell::Bash, &mut app, &outdir, "bash/comon");
+    generate(Shell::Elvish, &mut app, &outdir, "elvish/comon");
+    generate(Shell::Fish, &mut app, &outdir, "fish/comon");
+    generate(Shell::PowerShell, &mut app, &outdir, "powershell/comon");
+    generate(Shell::Zsh, &mut app, &outdir, "zsh/_comon");
 }
-
